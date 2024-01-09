611875c3ccdcc53aa07f8e8f7ea04ae510fbe0ab
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 08:16:19 -0500

    added accordion item heading and accordion item panel tags within the accordion item tag

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,12 +1,17 @@
 import { Accordion, AccordionItem } from "react-accessible-accordion";
-
+import { AccordionItemHeading } from "react-accessible-accordion";
 const Forecast = ({data}) => {
     return (
     <>
         <label className="title">Daily</label>
         <Accordion allowZeroExpanded>
-            {data.list.slice(0, 7).map((item, idx))}
-            <AccordionItem></AccordionItem>
+            {data.list.slice(0, 7).map((item, idx) => (
+                <AccordionItem>
+                    <AccordionItemHeading></AccordionItemHeading>
+                    <AccordionItemPanel></AccordionItemPanel>
+                </AccordionItem>
+            ))}
+
         </Accordion>
     </>
     )
