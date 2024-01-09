bf32442c3ba24e320c5416220327f646afec389f
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 06:52:58 -0500

    adding fetch url and importing the url and key from api to app js file

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Search from './components/search/search'
 import CurrentWeather from './components/search/current-weather/current-weather'
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 
 function App() {
 
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData)
+   const [lat, lon] = searchData.value.split(" ");
+
+   const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`)
+
   }
   const [count, setCount] = useState(0)
 
