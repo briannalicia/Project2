40a07404d01c0ed4d0605f471a38020ca6321796
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 08:36:08 -0500

    added an array of the days of the week in the forecast js file and used the slice method to display them

--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -1,19 +1,32 @@
-import { Accordion, AccordionItem } from "react-accessible-accordion";
-import { AccordionItemHeading } from "react-accessible-accordion";
-const Forecast = ({data}) => {
-    return (
-    <>
-        <label className="title">Daily</label>
-        <Accordion allowZeroExpanded>
-            {data.list.slice(0, 7).map((item, idx) => (
-                <AccordionItem>
-                    <AccordionItemHeading></AccordionItemHeading>
-                    <AccordionItemPanel></AccordionItemPanel>
-                </AccordionItem>
-            ))}
+import { Accordion, AccordionItem, AccordionItemButton, AccordionItemHeading, AccordionItemPanel } from "react-accessible-accordion";
 
-        </Accordion>
-    </>
+const WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+const Forecast = ({ data }) => {
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+    console.log(forecastDays);
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+                {data.list.slice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src={`icons/${item.weather[0].icon}.png`} />
+                                    <label className="day"></label>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel></AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+
+            </Accordion>
+        </>
     )
 }
 
