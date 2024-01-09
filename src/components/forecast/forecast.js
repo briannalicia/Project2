afb13fbad8f473c9690c2701e7d78e1d4a3ad83e
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 08:00:23 -0500

    added accordion for daily forecast in the forecast js file

---
+++ src/components/forecast/forecast.js
@@ -0,0 +1,15 @@
+import { Accordion } from "react-accessible-accordion";
+
+const Forecast = () => {
+    return (
+    <>
+        <label className="title">Daily</label>
+        <Accordion allowMultipleExpanded>
+
+        </Accordion>
+    </>
+    )
+}
+
+
+export default Forecast;
