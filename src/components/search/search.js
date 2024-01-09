f87e9fdd5ee6e2a89902105fd1967e087df47246
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 05:28:20 -0500

    added latitude and longitude values for api fetch function

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -10,7 +10,16 @@ const Search = ({onSearchChange}) => {
         return fetch(`${GEO_API_URL}/cities?minPopulation=1000000namePrefix${inputValue}`, geoApiOptions)
 
         .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => {
+            return {
+                options: response.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name, city.countryCode}`,
+                    }
+                })
+            }
+        })
         .catch(err => console.error(err));
     }
 
