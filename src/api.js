7a58519539d3a757be1cdcf031b70a3b530c012c
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 06:49:01 -0500

    adding additional api url and api key to api js file

--- src/api.js
+++ src/api.js
@@ -8,3 +8,5 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/3.0/"
+export const WEATHER_API_KEY = "53600a2412ce206e9303a2e7d695c4d8"
