e4b33bbd5157edfc5a3335ea1643f0b58182a3bc
Brianna <briannaa.kennedy@gmail.com>
2024-01-09 07:14:28 -0500

    adding promise all function to call new consts for display

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,23 @@ import CurrentWeather from './components/search/current-weather/current-weather'
 import { WEATHER_API_URL, WEATHER_API_KEY } from './api'
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
    const [lat, lon] = searchData.value.split(" ");
 
    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`)
+   const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&exclude={part}&appid=${WEATHER_API_KEY}`)
+
+   Promise.all([currentWeatherFetch, forecastFetch])
+   .then(async (response) => {
+    const weatherResponse = await response[0].json();
+    const forecastResponse = await response[1].json();
+    setCurrentWeather({weatherResponse});
+    setForecast({forecastResponse});
+   })
+  //  api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
   }
   const [count, setCount] = useState(0)
